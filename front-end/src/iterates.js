a243733aaebb59326f7425e6e0e0fcb1f6757edf
ahmed-z5645 <ahmed.zafar5645@gmail.com>
2024-08-08 15:53:26 -0400

    v0.1.2: fixed font issue, changed the way images are loaded(from public rather than url) and changed background colour to avoid ugly white flash when image has not yet loaded. Still need to work on image loading speed and process, but this should be good for now.

--- front-end/src/iterates.js
+++ front-end/src/iterates.js
@@ -1,60 +1,60 @@
 export const backgrounds = [
     {
-        url: "https://images.pexels.com/photos/3709434/pexels-photo-3709434.jpeg",
+        url: "background_photos/back1.jpeg",
         photographer: "Adrien Olichon",
         quote: "Productivity is being able to do things that you were never able to do before.",
         author: "Franz Kafka"
     },
     {
-        url: "https://images.pexels.com/photos/4175158/pexels-photo-4175158.jpeg",
+        url: "background_photos/back2.jpeg",
         photographer: "Rok Romih",
         quote: "You may delay, but time will not.",
         author: "Benjamin Franklin"
     },
     {
-        url: "https://images.pexels.com/photos/1766838/pexels-photo-1766838.jpeg",
+        url: "background_photos/back3.jpeg",
         photographer: "Baskin Creative Co.",
         quote: "Where your attention goes, your time goes.",
         author: "Idowu Koyenikan"
     },
     {
-        url: "https://images.pexels.com/photos/1324803/pexels-photo-1324803.jpeg",
+        url: "background_photos/back4.jpeg",
         photographer: "Matthew Montrone",
         quote: "Action is the foundational key to all success.",
         author: "Pablo Picasso"
     },
     {
-        url: "https://images.pexels.com/photos/1519088/pexels-photo-1519088.jpeg",
+        url: "background_photos/back5.jpeg",
         photographer: "James Wheeler",
         quote: "Whatever can happen any time can happen today.",
         author: "Seneca"
     },
     {
-        url: "https://images.pexels.com/photos/1169754/pexels-photo-1169754.jpeg",
+        url: "background_photos/back6.jpeg",
         photographer: "Philippe Donn",
         quote: "If you spend too much time thinking about a thing, you'll never get it done.",
         author: "Bruce Lee"
     },
     {
-        url: "https://images.pexels.com/photos/1612351/pexels-photo-1612351.jpeg",
+        url: "background_photos/back7.jpeg",
         photographer: "eberhard grossgasteiger",
         quote: "Working on the right thing is probably more important than working hard.",
         author: "Caterina Fake"
     },
     {
-        url: "https://images.pexels.com/photos/707915/pexels-photo-707915.jpeg",
+        url: "background_photos/back8.jpeg",
         photographer: "Fabian Reitmeier",
         quote: "Objects in motion tend to stay in motion. Find a way to get started in less than two minutes.",
         author: "James Clear"
     },
     {
-        url: "https://images.pexels.com/photos/2559941/pexels-photo-2559941.jpeg",
+        url: "background_photos/back9.jpeg",
         photographer: "Roberto Nickson",
         quote: "Light tomorrow with today.",
         author: "Elizabeth Barrett Browning"
     },
     {
-        url: "https://images.pexels.com/photos/2049422/pexels-photo-2049422.jpeg",
+        url: "background_photos/back10.jpeg",
         photographer: "Pok Rie",
         quote: "Success is the sum of small things done day in and day out.",
         author: "Robert Collier"
