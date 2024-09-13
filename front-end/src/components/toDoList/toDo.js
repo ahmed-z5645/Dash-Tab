a84355a824663ff1722201996ac3bfa9061d87c8
ahmed-z5645 <ahmed.zafar5645@gmail.com>
2024-09-13 16:10:49 -0400

    display raw data for all the inputted tasks on the browser

--- front-end/src/components/toDoList/toDo.js
+++ front-end/src/components/toDoList/toDo.js
@@ -11,6 +11,7 @@ import LoginInterface from './auth.js'
 const ToDoList = () => {
     const [isLoggedIn, setIsLoggedIn] = useState(true)
     const [needCreate, setNeedCreate] = useState(false);
+    const [userInfo, setUserInfo] = useState(null)
      
     useEffect(()=>{
         const unsubscribe = onAuthStateChanged(auth, (user) => {
@@ -27,11 +28,15 @@ const ToDoList = () => {
     const getAllDocs = async(user) =>{
         const docRef = doc(db, "users", user.uid)
         const docSnap = await getDoc(docRef)
-
-        if (docSnap.exists()) {
-            console.log("Document data:", docSnap.data());
-        } else {
-            console.log("No such document!")
+        
+        if (!userInfo){
+            console.log('here')
+            if (docSnap.exists()) {
+                console.log("Document data:", docSnap.data());
+                setUserInfo(docSnap.data().tasks)
+            } else {
+                console.log("No such document!")
+            }
         }
     }
 
@@ -48,6 +53,29 @@ const ToDoList = () => {
             {(isLoggedIn) ? 
             <div> 
                 <div>signed in</div> 
+                <div className="to-do-ui">{(userInfo) ? 
+                    <div>
+                        <div className="new-to-do"></div>
+                        <div className="all-to-do">
+                            {Object.values(userInfo).map((item) => {
+                                console.log(item)
+                                console.log(userInfo)
+                                return(
+                                    <div className="task">
+                                        <div className="task-text">
+                                            <h1>{item.Title}</h1>
+                                            <h3>{item.tOfCreate.seconds}</h3>
+                                            <p>hello</p>
+                                        </div>
+                                        <div className="task-done">
+                                            <button>Completed</button>
+                                        </div>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    </div> 
+                    : <p>not accessing object properly</p>}</div>
                 <div className="login-create" onClick={logout}> Sign-Out </div>
             </div>
             :
